b9814ac6e463d3567bfe09a6be0937f0440801de
fsAlfian <alfian.horis@mcm.id>
2024-10-10 09:27:55 +0700

    edit service and repository to show todolist

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -8,4 +8,20 @@ namespace Repository{
         function remove (Todolist $todolist) : void;
         function findAll() : array;
     }
+
+    class TodolistRepositoryImpl implements TodolistRepository{
+
+        private array $todolist = array();
+        
+        function save (Todolist $todolist) : void{
+            
+        }
+
+        function remove (Todolist $todolist) : void{
+        }
+
+        function findAll() : array{
+            return $this->todolist;
+        }
+    }
 }
--- Service/TodolistService.php
+++ Service/TodolistService.php
@@ -1,11 +1,28 @@
 <?php
 
 namespace Service {
+    use Repository\TodolistRepository;
 
     interface TodolistService {
         function showTodolist();
         function addTodolist(string $todo);
         function removeTodolist(int $number);
+    }
+
+    class TodolistServiceImpl implements TodolistService {
+
+        private  TodolistRepository $todoListRepository;
+
+        public function __construct(TodolistRepository $todoListRepository) {
+            $this->todoListRepository = $todoListRepository;
+        }
+
+        function showTodolist() : void {
+            echo "TODOLIST" . PHP_EOL;
+            foreach ($this->todoListRepository->findAll() as $number => $value) {
+                echo "$number. $value" . PHP_EOL;
+            }
+        }
     }
 
 }
