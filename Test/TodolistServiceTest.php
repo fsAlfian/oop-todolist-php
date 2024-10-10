7dbb56182ebe2f3d461b3ba02c8dd167f3212368
fsAlfian <alfian.horis@mcm.id>
2024-10-10 10:12:14 +0700

    add test service todolist

--- Test/TodolistServiceTest.php
+++ Test/TodolistServiceTest.php
@@ -9,10 +9,10 @@ use Repository\TodolistRepositoryImpl;
 
 function testShowTodolist(): void{
     $todoListRepository = new TodolistRepositoryImpl();
-    $todoListRepository->todoList[] = "Belajar PHP Dasar";
-    $todoListRepository->todoList[] = "Belajar PHP OOP";
-    $todoListRepository->todoList[] = "Belajar PHP Database";
-    $todoListRepository->todoList[] = "Belajar PHP Framework";
+    $todoListRepository->todoList[1] = "Belajar PHP Dasar";
+    $todoListRepository->todoList[2] = "Belajar PHP OOP";
+    $todoListRepository->todoList[3] = "Belajar PHP Database";
+    $todoListRepository->todoList[4] = "Belajar PHP Framework";
 
     $todoListService = new TodolistServiceImpl($todoListRepository);
     $todoListService->showTodolist();
