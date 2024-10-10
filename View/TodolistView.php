cab90859f51a5108ed47915569bb143ff6da487f
fsAlfian <alfian.horis@mcm.id>
2024-10-10 12:12:47 +0700

    add view todo list and add helper input

--- View/TodolistView.php
+++ View/TodolistView.php
@@ -1,9 +1,35 @@
 <?php
 
 namespace View {
+    use Service\TodoListService;
+    use Helper\InputHelper;
     class TodolistView {
+        private TodoListService $todoListService;
+
+        public function __construct(TodoListService $todoListService) {
+            $this->todoListService = $todoListService;
+        }
         function showTodolist() : void {
+            while(true) {
+                $this->todoListService->showTodolist();
+                echo '"Menu"' . PHP_EOL;
+                echo "1. Add Todo" . PHP_EOL;
+                echo "2. Remove Todo" . PHP_EOL;
+                echo "3. Exit" . PHP_EOL;
             
+                $choice = InputHelper::input("Input your choice");
+
+                if ($choice == "1") {
+                    $this->addTodolist();
+                } else if ($choice == "2") {
+                    $this->removeTodolist();
+                } else if ($choice == "3") {
+                    // exit
+                    break;
+                } else {
+                    echo "Wrong choice" . PHP_EOL;
+                }
+            }
         }
 
         function addTodolist() : void {
